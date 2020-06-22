f4a5654d291ce56d1608b29f341f6cd4fa4f57fe
L.Meijerink <lotta.meijerink@pacmed.nl>
2020-06-22 17:21:11 +0200

    working experiment

--- uncertainty_estimation/experiments/OOD_experiment.py
+++ uncertainty_estimation/experiments/OOD_experiment.py
@@ -1,39 +1,14 @@
 import pandas as pd
 import os
 import pickle
-from uncertainty_estimation.experiments_utils.ood_experiments_utils import NoveltyAnalyzer, \
-    split_by_ood_name
 
 from uncertainty_estimation.models.novelty_estimator_wrapper import NoveltyEstimator
-from sklearn import tree
-from sklearn import pipeline
-from sklearn.metrics import roc_auc_score
 from sklearn import svm
-
 from sklearn.decomposition import PCA
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn import neighbors
 
 from uncertainty_estimation.models.autoencoder import AE
-
-OOD_MAPPINGS = {'Emergency/\nUrgent admissions': ('ADMISSION_TYPE', 'EMERGENCY'),
-                'Elective admissions': ('ADMISSION_TYPE', 'ELECTIVE'),
-                # 'Ethnicity: Asian': ('Ethnicity', 1),
-                'Ethnicity: Black/African American': ('Ethnicity', 2),
-                # 'Ethnicity: Hispanic/Latino': ('Ethnicity', 3),
-                'Ethnicity: White': ('Ethnicity', 4),
-                'Female': ('GENDER', 'F'),
-                'Male': ('GENDER', 'M'),
-                'Thyroid disorders': ('Thyroid disorders', True),
-                'Acute and unspecified renal failure': (
-                    'Acute and unspecified renal failure', True),
-                # 'Pancreatic disorders \n(not diabetes)': (
-                # 'Pancreatic disorders (not diabetes)', True),
-                'Epilepsy; convulsions': ('Epilepsy; convulsions', True),
-                'Hypertension with complications \n and secondary hypertension': (
-                    'Hypertension with complications and secondary hypertension', True)}
+import uncertainty_estimation.experiments_utils.ood_experiments_utils as ood_utils
 
 processed_folder = "/data/processed/benchmark/inhospitalmortality/not_scaled"
 val_data = pd.read_csv(os.path.join(processed_folder, 'val_data_processed_w_static.csv'),
@@ -45,48 +20,52 @@ test_data = pd.read_csv(os.path.join(processed_folder, 'test_data_processed_w_st
 other_data = pd.read_csv(os.path.join(processed_folder, 'other_data_processed_w_static.csv'),
                          index_col=0)
 
-with open('MIMIC_utils/feature_names.pkl', 'rb') as f:
+with open('../experiments_utils/MIMIC_feature_names.pkl', 'rb') as f:
     feature_names = pickle.load(f)
 
-
-model = AE
-model_params = dict(
+ae = NoveltyEstimator(AE, dict(
     input_dim=len(feature_names),
     hidden_dims=[5],
     latent_dim=2,
     batch_size=256,
-    learning_rate=0.001)
+    learning_rate=0.001), dict(n_epochs=30), 'AE')
 
-train_params = dict(n_epochs=30)
-ne = NoveltyEstimator(model, model_params, train_params, 'AE')
-print("Newborns")
-newborns = other_data[other_data["ADMISSION_TYPE"] == 'NEWBORN']
-nov_an = NoveltyAnalyzer(ne, train_data[feature_names],
-                         test_data[feature_names],
-                         newborns[feature_names], impute_and_scale=True)
-nov_an.calculate_novelty()
-print("Recalled OOD fraction is {:.2f}".format(nov_an.get_ood_recall()))
-print("OOD detection AUC is {:.2f}".format(nov_an.get_ood_detection_auc()))
+pca = NoveltyEstimator(PCA, dict(n_components=3), {}, 'sklearn')
+svm = NoveltyEstimator(svm.OneClassSVM, {}, {}, 'sklearn')
 
+lof = NoveltyEstimator(neighbors.LocalOutlierFactor, dict(novelty=True), {}, 'sklearn')
 
-for ood_name, (column_name, ood_value) in OOD_MAPPINGS.items():
-    # Split all data splits into OOD and 'Non-OOD' data.
-    train_ood, train_non_ood = split_by_ood_name(train_data, column_name, ood_value)
-    val_ood, val_non_ood = split_by_ood_name(val_data, column_name, ood_value)
-    test_ood, test_non_ood = split_by_ood_name(val_data, column_name, ood_value)
+if __name__ == '__main__':
+    for ne in [ae, pca, lof, svm]:
+        print(ne.model_type.__name__)
+        print("Newborns")
+        newborns = other_data[other_data["ADMISSION_TYPE"] == 'NEWBORN']
+        nov_an = ood_utils.NoveltyAnalyzer(ne, train_data[feature_names],
+                                           test_data[feature_names],
+                                           newborns[feature_names], impute_and_scale=True)
+        nov_an.calculate_novelty()
+        print("Recalled OOD fraction is {:.2f}".format(nov_an.get_ood_recall()))
+        print("OOD detection AUC is {:.2f}".format(nov_an.get_ood_detection_auc()))
 
-    # Group all OOD splits together.
-    all_ood = pd.concat([train_ood, test_ood, val_ood])
-    print("\n" + ood_name)
-    min_size = min(len(all_ood), len(test_non_ood))
+        for ood_name, (column_name, ood_value) in ood_utils.MIMIC_OOD_MAPPINGS.items():
+            # Split all data splits into OOD and 'Non-OOD' data.
+            train_ood, train_non_ood = ood_utils.split_by_ood_name(train_data, column_name,
+                                                                   ood_value)
+            val_ood, val_non_ood = ood_utils.split_by_ood_name(val_data, column_name, ood_value)
+            test_ood, test_non_ood = ood_utils.split_by_ood_name(val_data, column_name, ood_value)
 
-    test_non_ood = test_non_ood.sample(min_size)
-    all_ood = all_ood.sample(min_size)
+            # Group all OOD splits together.
+            all_ood = pd.concat([train_ood, test_ood, val_ood])
+            print("\n" + ood_name)
+            min_size = min(len(all_ood), len(test_non_ood))
 
-    nov_an = NoveltyAnalyzer(ne, train_non_ood[feature_names],
-                             test_non_ood[feature_names],
-                             all_ood[feature_names], impute_and_scale=True)
+            test_non_ood = test_non_ood.sample(min_size)
+            all_ood = all_ood.sample(min_size)
 
-    nov_an.calculate_novelty()
-    print("Recalled OOD fraction is {:.2f}".format(nov_an.get_ood_recall()))
-    print("OOD detection AUC is {:.2f}".format(nov_an.get_ood_detection_auc()))
+            nov_an = ood_utils.NoveltyAnalyzer(ne, train_non_ood[feature_names],
+                                               test_non_ood[feature_names],
+                                               all_ood[feature_names], impute_and_scale=True)
+
+            nov_an.calculate_novelty()
+            print("Recalled OOD fraction is {:.2f}".format(nov_an.get_ood_recall()))
+            print("OOD detection AUC is {:.2f}".format(nov_an.get_ood_detection_auc()))
