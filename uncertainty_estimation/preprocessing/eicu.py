b4414b1f238cca8205d30816d0b6d8b82b825c5b
Kaleidophon <Dennis.Ulmer@gmx.de>
2020-06-29 14:06:25 +0200

    :sparkles: Finish feature engineering pipeline

--- uncertainty_estimation/preprocessing/eicu.py
+++ uncertainty_estimation/preprocessing/eicu.py
@@ -8,7 +8,6 @@ Afterwards, the information is processed akin to the MIMIC-III data set as descr
 
 """
 
-# TODO: Extract time series features
 # TODO: Split into training / test 85 % / 15 %
 # TODO: Create OOD cohorts
 #   - Newborns (need to re-run script)
@@ -25,12 +24,12 @@ Afterwards, the information is processed akin to the MIMIC-III data set as descr
 
 import argparse
 import itertools
-import os
 from typing import Dict, Union
 
 import numpy as np
 import pandas as pd
 from scipy.stats import skew
+from tqdm import tqdm
 
 # Custom types
 TimeSeriesFeatures = Dict[str, Union[int, float]]
@@ -43,6 +42,25 @@ TIME_SERIES_VARS = [
 	"FiO2", "Heart Rate", "Invasive BP Diastolic", "Invasive BP Systolic", "O2 Saturation", "Respiratory Rate",
 	"Temperature (C)", "glucose", "Motor", "Eyes", "MAP (mmHg)", "GCS Total", "Verbal", "pH"
 ]
+PHENOTYPE_TO_ICD9 = {
+	"Thyroid disorders": {
+		"2400", "2409", "2410", "2411", "2419", "24200", "24201", "24210", "24211", "24220", "24221", "24230", "24231",
+		"24240", "24241", "24280", "24281", "24290", "24291", "243", "2440", "2441", "2442", "2443", "2448", "2449",
+		"2450", "2451", "2452", "2453", "2454", "2458", "2459", "2460", "2461", "2462", "2463", "2468", "2469", "7945"
+	},
+	"Acute and unspecified renal failure": {"5845", "5846", "5847", "5848", "5849", "586"},
+	"Epilepsy; convulsions": {
+		"3450", "34500", "34501", "3451", "34510", "34511", "3452", "3453", "3454", "34540", "34541", "3455", "34550",
+		"34551", "3456", "34560", "34561", "3457", "34570", "34571", "3458", "34580", "34581", "3459", "34590", "34591",
+		"7803", "78031", "78032", "78033", "78039"
+	},
+	"Hypertension with complications and secondary hypertension": {
+		"4010", "40200", "40201", "40210", "40211", "40290", "40291", "4030", "40300", "40301", "4031", "40310",
+		"40311", "4039", "40390", "40391", "4040", "40400", "40401", "40402", "40403", "4041", "40410", "40411",
+		"40412", "40413", "4049", "40490", "40491", "40492", "40493", "40501", "40509", "40511", "40519", "40591",
+		"40599", "4372"
+	}
+}
 
 
 def engineer_features(data_path: str, diagnoses_path: str, output_dir: str):
@@ -51,20 +69,26 @@ def engineer_features(data_path: str, diagnoses_path: str, output_dir: str):
 
 	Parameters
 	----------
-	data_path
-	diagnoses_path
-	output_dir
+	data_path: str
+		Path to all_data.csv file.
+	diagnoses_path: str
+		Path to (original) diagnosis.csv file.
+	output_dir: str
+		Path to output directory.
 
 	Returns
 	-------
-
+	engineered_data: pd.DataFrame
+		Data for every patient stay with new, engineered features.
 	"""
 	all_data = read_all_data(data_path)
-	engineered_data = pd.DataFrame(columns=["patientunitstayid"] + STATIC_VARS)
+	diagnoses_data = read_diagnoses_file(diagnoses_path)
+
+	engineered_data = pd.DataFrame(columns=["patientunitstayid"] + STATIC_VARS + list(PHENOTYPE_TO_ICD9.keys()))
 	engineered_data = engineered_data.set_index("patientunitstayid")
 
-	# 1. Add all static features
-	for stay_id in all_data["patientunitstayid"].unique():
+	for stay_id in tqdm(all_data["patientunitstayid"].unique()):
+		# 1. Add all static features to the new table
 		engineered_data.loc[stay_id] = all_data[all_data["patientunitstayid"] == stay_id][STATIC_VARS].iloc[0]
 
 		# 2. Get features for all time series variables
@@ -83,7 +107,22 @@ def engineer_features(data_path: str, diagnoses_path: str, output_dir: str):
 				engineered_data.loc[stay_id][feat] = val
 
 		# 3. Get phenotype features
-		# TODO
+		diagnoses = diagnoses_data[
+			(diagnoses_data["patientunitstayid"] == stay_id) &
+			(~diagnoses_data["icd9code"].isna())
+		]["icd9code"]
+		diagnoses = {parse_icd9code(code) for code in diagnoses}
+
+		for phenotype, codes in PHENOTYPE_TO_ICD9.items():
+			# Check whether there is any overlap in the ICD9 codes corresponding to the diagnoses given during the stay
+			# and the codes belonging to the current phenotype
+			engineered_data.loc[stay_id][phenotype] = int(len(diagnoses & codes) > 0)
+
+	return engineered_data
+
+
+def parse_icd9code(raw_icd9code: str) -> str:
+	return raw_icd9code.split(",")[0].replace(".", "")
 
 
 def get_time_series_features(time_series: pd.Series, var_name: str) -> TimeSeriesFeatures:
@@ -184,6 +223,33 @@ def read_all_data(data_path: str) -> pd.DataFrame:
 			"patientunitstayid": int,
 			"unitdischargeoffset": int,
 			"unitdischargestatus": int
+		}
+	)
+
+
+def read_diagnoses_file(diagnoses_path: str) -> pd.DataFrame:
+	"""
+	Read the original eICU diagnosis.csv.
+
+	Parameters
+	----------
+	diagnoses_path: str
+		Path to (original) diagnosis.csv file.
+
+	Returns
+	-------
+	diagnoses_data: pd.DataFrame
+		Data about patient diagnoses.
+	"""
+	return pd.read_csv(
+		diagnoses_path, dtype={
+			"diagnosisid": int,
+			"patientunitstayid": int,
+			"activeupondischarge": bool,
+			"diagnosisoffset": int,
+			"diagnosisstring": str,
+			"icd9code": str,
+			"diagnosispriority": str
 		}
 	)
 
