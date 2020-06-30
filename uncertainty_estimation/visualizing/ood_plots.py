ef3c69e3a11efd5591fb231c5c897c86bb9c880a
L.Meijerink <lotta.meijerink@pacmed.nl>
2020-06-30 15:23:35 +0200

    Added metrics and visualizations for confidence/performance plots

---
+++ uncertainty_estimation/visualizing/ood_plots.py
@@ -0,0 +1,51 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def barplot_from_nested_dict(nested_dict, metric_name='OOD detection AUC',
+                             group_name='OOD group', vline=None, xlim=(0, 1.0), save_dir=None,
+                             height=6,
+                             aspect=1.5, legend_out=False):
+    sns.set_style('whitegrid')
+    df = pd.DataFrame.from_dict(nested_dict)
+
+    df = df.stack().reset_index()
+    df.columns = [group_name, '', metric_name]
+
+    sns.catplot(x=metric_name, y=group_name, hue='', data=df, kind='bar',
+                height=height, aspect=aspect, facet_kws=dict(despine=False), alpha=0.9,
+                legend_out=legend_out)
+    plt.xlim(xlim)
+    if vline:
+        plt.axvline(vline, linestyle='--')
+    if save_dir:
+        plt.savefig(save_dir, dpi=300,
+                    bbox_inches='tight', pad=0)
+        plt.close()
+    else:
+        plt.show()
+
+
+def boxplot_from_nested_listdict(nested_dict, name, hline=None, ylim=(0.0, 1.0), x_name='scale',
+                                 save_dir=None, **kwargs):
+    sns.set_palette("Set1", 10)
+    sns.set_style('whitegrid')
+    df = pd.DataFrame.from_dict(nested_dict,
+                                orient='columns')
+
+    df = df.stack().reset_index()
+    df.columns = [x_name, '', name]
+    df = df.explode(name)
+
+    sns.catplot(x=x_name, y=name, hue='', data=df, kind='box',
+                facet_kws=dict(despine=False), legend_out=False, **kwargs)
+    plt.ylim(ylim)
+    if hline:
+        plt.axhline(hline, linestyle='--')
+    if save_dir:
+        plt.savefig(save_dir, dpi=300,
+                    bbox_inches='tight', pad=0)
+        plt.close()
+    else:
+        plt.show()
