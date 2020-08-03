0a89aabbfecaadd91a9dff0d5f5870e6fd1c05a5
Kaleidophon <Dennis.Ulmer@gmx.de>
2020-08-03 12:03:00 +0200

    :recycle: Finish rewriting part of refactor

--- uncertainty_estimation/experiments/in_domain.py
+++ uncertainty_estimation/experiments/in_domain.py
@@ -1,24 +1,43 @@
+"""
+Perform novelty estimation experiments on in-domain data.
+"""
+
+# STD
 import os
 import pickle
 import argparse
+from collections import defaultdict
+
+# EXT
 from sklearn.impute import SimpleImputer
 from sklearn import pipeline
 from sklearn.preprocessing import StandardScaler
-from experiments_utils import get_models_to_use
-from experiments_utils.datahandler import DataHandler
 import numpy as np
 import torch
 
-# TODO: Comments, formatting
-# TODO: Avoid repeated lists
+# PROJECT
+from uncertainty_estimation.models.info import NEURAL_PREDICTORS, AVAILABLE_MODELS
+from uncertainty_estimation.utils.model_init import init_models
+from uncertainty_estimation.utils.datahandler import DataHandler
 
+# CONST
 N_SEEDS = 5
+SEED = 123
+
 if __name__ == "__main__":
-    np.random.seed(123)
-    torch.manual_seed(123)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--data_origin", type=str, default="eICU", help="Which data to use"
+    )
+    parser.add_argument(
+        "--models",
+        type=str,
+        nargs="+",
+        default=AVAILABLE_MODELS,
+        choices=AVAILABLE_MODELS,
+        help="Determine the models which are being used for this experiment.",
     )
     args = parser.parse_args()
 
@@ -38,50 +57,50 @@ if __name__ == "__main__":
     X_test = pipe.transform(test_data[feature_names])
     X_val = pipe.transform(val_data[feature_names])
 
-    uncertainties = dict()
-    for ne, kinds, method_name in get_models_to_use(len(feature_names)):
-        print(method_name)
-        for kind in kinds:
-            uncertainties[kind] = []
+    uncertainties = defaultdict(lambda: defaultdict(list))
 
+    for ne, scoring_funcs, method_name in init_models(
+        input_dim=len(feature_names), selection=AVAILABLE_MODELS
+    ):
+        print(method_name)
         predictions = []
+
         for i in range(N_SEEDS):
             ne.train(X_train, train_data[y_name].values, X_val, val_data[y_name].values)
-            for kind in kinds:
-                uncertainties[kind] += [ne.get_novelty_score(X_test, scoring_func=kind)]
-                print(len(uncertainties[kind][0]))
-            if method_name in [
-                "Single_NN",
-                "NN_Ensemble",
-                "MC_Dropout",
-                "NN_Ensemble_bootstrapped",
-            ]:
+
+            for scoring_func in scoring_funcs:
+                uncertainties[scoring_func] += [
+                    ne.get_novelty_score(X_test, scoring_func=scoring_func)
+                ]
+                print(len(uncertainties[scoring_func][0]))
+
+            if method_name in NEURAL_PREDICTORS:
                 predictions += [ne.model.predict_proba(X_test)[:, 1]]
 
         dir_name = os.path.join("pickled_results", args.data_origin, "ID", method_name)
+
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
 
         uncertainties_dir_name = os.path.join(dir_name, "uncertainties")
+
         if not os.path.exists(uncertainties_dir_name):
             os.mkdir(uncertainties_dir_name)
+
         predictions_dir_name = os.path.join(dir_name, "predictions")
+
         if not os.path.exists(predictions_dir_name):
             os.mkdir(predictions_dir_name)
-        for kind in kinds:
-            with open(
-                os.path.join(uncertainties_dir_name, str(kind) + ".pkl"), "wb"
-            ) as f:
-                pickle.dump(uncertainties[kind], f)
 
-        if method_name in [
-            "Single_NN",
-            "NN_Ensemble",
-            "MC_Dropout",
-            "NN_Ensemble__bootstrapped",
-            "NN_Ensemble_anchored",
-        ]:
+        for scoring_func in scoring_funcs:
+            with open(
+                os.path.join(uncertainties_dir_name, str(scoring_func) + ".pkl"), "wb"
+            ) as f:
+                pickle.dump(uncertainties[scoring_func], f)
+
+        if method_name in NEURAL_PREDICTORS:
             predictions = ne.model.predict_proba(X_test)[:, 1]
+
             with open(os.path.join(predictions_dir_name, "predictions.pkl"), "wb") as f:
                 pickle.dump(predictions, f)
     with open(
