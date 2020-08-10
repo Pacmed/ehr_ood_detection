:bug: Fix bug with platt scaling, update results

uncertainty_estimation/models/novelty_estimator.py
@@ -87,7 +87,10 @@ class NoveltyEstimator:
         np.ndarray
             The novelty estimates.
         """
-        self.model.eval()
+        try:
+            self.model.eval()
+        except AttributeError:
+            pass
 
         if self.name == "AE":
             return self.model.get_reconstr_error(data)

uncertainty_estimation/utils/metrics.py
@@ -73,8 +73,9 @@ def max_abs_out(probabilities: np.array, axis: int) -> Union[float, np.array]:
     inverse_sigmoid = lambda probs: np.log(
         np.array(probs) / (1 + 1e-5 - np.array(probs)) + 1e-5
     )
+    max_abs_out_vals = np.max(np.abs(inverse_sigmoid(probabilities)), axis)
 
-    return np.max(np.abs(inverse_sigmoid(probabilities)), axis)
+    return max_abs_out_vals
 
 
 def entropy(probabilities: np.array, axis: int) -> Union[float, np.array]: