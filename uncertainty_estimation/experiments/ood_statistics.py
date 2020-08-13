5b0e5303870713692e113f3e3e0ecb246eba408b
Kaleidophon <Dennis.Ulmer@gmx.de>
2020-08-13 17:49:26 +0200

    :sparkles: Add statistics about relative size of OOD groups and percentage of diff. features to plots

--- uncertainty_estimation/experiments/ood_statistics.py
+++ uncertainty_estimation/experiments/ood_statistics.py
@@ -3,7 +3,10 @@ Get some statistics about ood groups.
 """
 
 # STD
+import argparse
 from collections import defaultdict
+import os
+import pickle as pkl
 
 # EXT
 import pandas as pd
@@ -12,8 +15,25 @@ import pandas as pd
 import uncertainty_estimation.utils.ood as ood_utils
 from uncertainty_estimation.utils.datahandler import DataHandler, BASE_ORIGINS
 
+# CONST
+STATS_DIR = "../../data/stats"
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_origin", type=str, default="MIMIC", help="Which data to use",
+    )
+    parser.add_argument(
+        "--stats-dir",
+        type=str,
+        default=STATS_DIR,
+        help="Define the directory that results should be saved to.",
+    )
+    args = parser.parse_args()
+
     df = defaultdict(lambda: defaultdict(dict))
+    data_origin = args.data_origin
 
     for data_origin in BASE_ORIGINS:
         dh = DataHandler(data_origin)
@@ -21,16 +41,25 @@ if __name__ == "__main__":
         train_data, test_data, val_data = dh.load_data_splits()
         y_name = dh.load_target_name()
         ood_mappings = dh.load_ood_mappings()
+        rel_sizes = {}
+        percentage_sigs = {}
 
         if data_origin == "MIMIC":
 
             train_ood, test_ood, val_ood = dh.load_newborns()
             all_ood = pd.concat([train_ood, test_ood, val_ood])
 
-            df[data_origin]["Newborns"]["Count"] = len(all_ood)
-            df[data_origin]["Newborns"]["Mortality rate"] = round(
-                all_ood["y"].mean(), 3
+            df[data_origin]["Newborn"]["Count"] = len(all_ood)
+            df[data_origin]["Newborn"]["Mortality rate"] = round(all_ood["y"].mean(), 3)
+
+            rel_sizes["Newborn"] = len(all_ood) / (
+                len(train_data) + len(test_data) + len(val_data)
             )
+            percentage_sigs["Newborn"] = ood_utils.validate_ood_data(
+                train_data[feature_names].values,
+                all_ood[feature_names].values,
+                verbose=False,
+            )[1]
 
         for ood_name, (column_name, ood_value) in ood_mappings:
             train_ood, train_non_ood = ood_utils.split_by_ood_name(
@@ -48,7 +77,25 @@ if __name__ == "__main__":
                 all_ood[y_name].mean(), 3
             )
 
+            rel_sizes[ood_name] = len(all_ood) / (
+                len(train_data) + len(test_data) + len(val_data)
+            )
+            percentage_sigs[ood_name] = ood_utils.validate_ood_data(
+                train_non_ood[feature_names].values,
+                all_ood[feature_names].values,
+                verbose=False,
+            )[1]
+
+        if not os.path.exists(f"{STATS_DIR}/{data_origin}/"):
+            os.makedirs(f"{STATS_DIR}/{data_origin}/")
+
         df_with_info = pd.concat(
             {k: pd.DataFrame.from_dict(v, "columns") for k, v in df.items()}, axis=0
         ).T
-        df_with_info.to_csv("../../data/ood_stats/ood_statistics.csv")
+        df_with_info.to_csv(f"{STATS_DIR}/{data_origin}/ood_statistics.csv")
+
+        with open(f"{STATS_DIR}/{data_origin}/rel_sizes.pkl", "wb") as f:
+            pkl.dump(rel_sizes, f)
+
+        with open(f"{STATS_DIR}/{data_origin}/percentage_sigs.pkl", "wb") as f:
+            pkl.dump(percentage_sigs, f)
--- uncertainty_estimation/visualizing/ood_plots.py
+++ uncertainty_estimation/visualizing/ood_plots.py
@@ -3,7 +3,7 @@ Module defining functions to plot the results of experiments.
 """
 
 # STD
-from typing import Optional
+from typing import Optional, Dict, Tuple
 
 # EXT
 import numpy as np
@@ -15,11 +15,13 @@ import pandas as pd
 from uncertainty_estimation.utils.types import ResultDict
 
 
-def heatmap_result_plot(
+def plot_results_as_heatmap(
     result_dict: ResultDict,
     name: str,
     lower_cmap_limit: float = 0,
     save_dir: Optional[str] = None,
+    rel_sizes: Optional[Dict[str, float]] = None,
+    percentage_sigs: Optional[Dict[str, float]] = None,
 ):
     """
     Plot results compactly inside a heatmap.
@@ -35,9 +37,36 @@ def heatmap_result_plot(
         be as good for random guessing - thus color everything below that the same way).
     save_dir: Optional[str]
         Path that figure should be saved to if given.
+    rel_sizes: Optional[Dict[str, float]]
+        Dictionary containing the relative sizes of groups models were tested on.
+    percentage_sigs: Optional[Dict[str, float]]
+        Dictionary containing the percentage of statistically significant different features of tested group compared to
+        reference group.
     """
     df = pd.DataFrame.from_dict(result_dict)
     df = df.sort_index()
+
+    if rel_sizes is not None or percentage_sigs is not None:
+        new_indices = []
+
+        for row_idx in df.index:
+            suffix = (
+                f"\n(size: {rel_sizes[row_idx] * 100:.2f} %"
+                if rel_sizes is not None
+                else "("
+            )
+            suffix += (
+                ", " if rel_sizes is not None and percentage_sigs is not None else ""
+            )
+            suffix += (
+                f"diff: {percentage_sigs[row_idx] * 100:.2f} %)"
+                if percentage_sigs is not None
+                else ")"
+            )
+            new_indices.append(f"{row_idx} {suffix}")
+
+        df.index = new_indices
+
     df = df.applymap(lambda l: np.array(l).mean())
     annotations = df.applymap(lambda l: f"{np.array(l).mean():.2f}".lstrip("0"))
 
@@ -66,69 +95,92 @@ def heatmap_result_plot(
         plt.show()
 
 
-def barplot_from_nested_dict(
-    nested_dict,
-    metric_name="OOD detection AUC",
-    group_name="OOD group",
-    vline=None,
-    xlim=(0, 1.0),
-    save_dir=None,
-    height=6,
-    aspect=1.5,
-    legend_out=False,
-):
-    sns.set_style("whitegrid")
-    sns.set_palette("Set1", 10)
-    df = pd.DataFrame.from_dict(nested_dict)
-
-    df = df.stack().reset_index()
-    df.columns = [group_name, "", metric_name]
-
-    sns.catplot(
-        x=metric_name,
-        y=group_name,
-        hue="",
-        data=df,
-        kind="bar",
-        height=height,
-        aspect=aspect,
-        facet_kws=dict(despine=False),
-        alpha=0.9,
-        legend_out=legend_out,
-    )
-    plt.xlim(xlim)
-    if vline:
-        plt.axvline(vline, linestyle="--")
-    if save_dir:
-        plt.savefig(save_dir, dpi=300, bbox_inches="tight", pad=0)
-        plt.close()
-    else:
-        plt.show()
-
-
-def boxplot_from_nested_listdict(
-    nested_dict,
-    name,
-    hline=None,
-    vline=None,
-    xlim=(0.0, 1.0),
-    ylim=(0.0, 1.0),
-    dummy_group_name=None,
-    x_name="scale",
-    horizontal=False,
-    legend_out=False,
-    figsize=None,
-    save_dir=None,
-    kind="box",
+def plot_results_as_boxplot(
+    result_dict: ResultDict,
+    name: str,
+    hline: Optional[float] = None,
+    vline: Optional[float] = None,
+    xlim: Optional[Tuple[float, float]] = (0.0, 1.0),
+    ylim: Optional[Tuple[float, float]] = (0.0, 1.0),
+    dummy_group_name: Optional[str] = None,
+    x_name: str = "scale",
+    horizontal: bool = False,
+    legend_out: bool = False,
+    figsize: Optional[Tuple[int, int]] = None,
+    save_dir: Optional[str] = None,
+    kind: str = "box",
+    rel_sizes: Optional[Dict[str, float]] = None,
+    percentage_sigs: Optional[Dict[str, float]] = None,
     **kwargs,
 ):
+    """
+    Plot results as a bar plot.
+
+    Parameters
+    ----------
+    result_dict: ResultDict
+        Results of experiments
+    name: str
+        Name of plot.
+    hline: Optional[float]
+        Plot a horizontal line at the given y position.
+    vline: Optional[float]
+        Plot a vertical line at the given x position.
+    xlim: Tuple[float, float]
+        Set the limits on the x-axis of the current plot.
+    ylim: TUple[float, float]
+        Set the limits on the y-axis of the current plot.
+    dummy_group_name: Optional[str]
+        Name of a dummy group used for padding.
+    x_name: str
+        Labels for the x-axis.
+    horizontal: bool
+        Whether to arrange the bars horizontally or vertically.
+    legend_out: bool
+        Whether the legend should be placed outside the figure.
+    figsize: Optional[Tuple[int, int]]
+        Size of the figure.
+    save_dir: Optional[str]
+        Path that figure should be saved to if given.
+    rel_sizes: Optional[Dict[str, float]]
+        Dictionary containing the relative sizes of groups models were tested on.
+    percentage_sigs: Optional[Dict[str, float]]
+        Dictionary containing the percentage of statistically significant different features of tested group compared to
+        reference group.
+    kind: str
+        The kind of plot to draw.
+    """
     sns.set_palette("Set1", 10)
     sns.set_style("whitegrid")
-    df = pd.DataFrame.from_dict(nested_dict, orient="columns")
+    df = pd.DataFrame.from_dict(result_dict, orient="columns")
+
+    if rel_sizes is not None or percentage_sigs is not None:
+        new_indices = []
+
+        for row_idx in df.index:
+            suffix = (
+                f"\n(size: {rel_sizes[row_idx] * 100:.2f} %"
+                if rel_sizes is not None
+                else "("
+            )
+            suffix += (
+                ", " if rel_sizes is not None and percentage_sigs is not None else ""
+            )
+            suffix += (
+                f"diff: {percentage_sigs[row_idx] * 100:.2f} %)"
+                if percentage_sigs is not None
+                else ")"
+            )
+            new_indices.append(f"{row_idx} {suffix}")
+
+        df.index = new_indices
 
     df = df.stack().reset_index()
+
     df.columns = [x_name, "", name]
     df = df.explode(name)
+    df = df.sort_index()
+
     if dummy_group_name:
         methods = [m for m in df[""].unique()]
         fake_df = pd.DataFrame(
@@ -139,7 +191,9 @@ def boxplot_from_nested_listdict(
             }
         )
         df = pd.concat([fake_df, df])
+
     plt.figure(figsize=figsize)
+
     if horizontal:
         sns.catplot(
             x=name,
@@ -166,6 +220,7 @@ def boxplot_from_nested_listdict(
 
     plt.ylim(ylim)
     plt.xlim(xlim)
+
     if hline:
         plt.axhline(hline, linestyle="--")
     if vline:
